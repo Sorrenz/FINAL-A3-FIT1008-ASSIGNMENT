713f42b803f9a2299b8c9f0fb0b540cd4bb48f2b
Sorrenz <shar0141@student.monash.edu>
2024-05-19 02:59:41 +0800

    Finished task 1

--- mode1.py
+++ mode1.py
@@ -1,6 +1,7 @@
 from landsites import Land
 from data_structures.bst import BinarySearchTree
 from algorithms.mergesort import mergesort
+from algorithms.binary_search import binary_search,_binary_search_aux
 
 class Mode1Navigator:
     """
@@ -16,6 +17,7 @@ class Mode1Navigator:
         binary_tree = BinarySearchTree()
         self.split(gold_adventureres_ratio, binary_tree)
         self.sites = binary_tree
+        self.sites.draw()
         self.adventurers = adventurers
         
     def split(self,lst,binary_tree):
@@ -60,15 +62,33 @@ class Mode1Navigator:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        gold_lst = []
+        for adventurers in adventure_numbers:
+            temp = adventurers
+            gold_discovered = 0
+            iterator = iter(self.sites)
+            for _ in range(len(self.sites)):
+                if temp > 0:
+                    island = iterator.__next__()
+                    gold_retrieved = min((temp*island.item.get_gold())/island.item.get_guardians(), island.item.get_gold())
+                    gold_discovered += gold_retrieved
+                    temp -= island.item.get_guardians()
+                else:
+                    break
+            gold_lst.append(gold_discovered)
+        return gold_lst
 
     def update_site(self, land: Land, new_reward: float, new_guardians: int) -> None:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
-    
-    
+        """if not self.sites[-(land.get_gold()/land.get_guardians())] in self.sites:
+            raise Exception("THE LAND IS NOT IN THE ISLAND LIST")"""
+        
+        del (self.sites[-(land.get_gold()/land.get_guardians())])
+        land.set_gold(new_reward)
+        land.set_guardians(new_guardians)
+        self.sites[-(land.get_gold()/land.get_guardians())] = land
     
 if __name__ == "__main__":
     a = Land("A", 400, 100)
@@ -84,10 +104,8 @@ if __name__ == "__main__":
         Land(c.get_name(), c.get_gold(), c.get_guardians()),
         Land(d.get_name(), d.get_gold(), d.get_guardians()),
         Land(e.get_name(), e.get_gold(), e.get_guardians()),
-        Land(f.get_name(), f.get_gold(), f.get_guardians())
     ]
     
-    #nav = Mode1Navigator(sites, 200)
-    nav = Mode1Navigator(sites, 0)
-    selected = nav.select_sites()
-    print(selected)
+    nav = Mode1Navigator(sites, 200)
+    results = nav.select_sites_from_adventure_numbers([0, 200, 500, 300, 40])
+    nav.update_site(sites[1], 400, 400)
